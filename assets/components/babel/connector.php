6e8235e9e4e1a55e48b10af597ff3e406477ab12
Romain <romain@melting-media.com>
2012-05-22 23:15:00 +0200

    First steps to rework

---
+++ assets/components/babel/connector.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @var modX $modx
+ */
+require_once dirname(dirname(dirname(dirname(__FILE__)))).'/config.core.php';
+require_once MODX_CORE_PATH.'config/'.MODX_CONFIG_KEY.'.inc.php';
+require_once MODX_CONNECTORS_PATH.'index.php';
+
+$babelCorePath = $modx->getOption('babel.core_path', null, $modx->getOption('core_path') .'components/babel/');
+require_once $babelCorePath.'model/babel/babel.class.php';
+
+$modx->babel = new Babel($modx);
+
+$modx->lexicon->load('babel:default');
+
+// handle request
+$path = $modx->getOption('processorsPath', $modx->babel->config, $babelCorePath.'processors/');
+$modx->request->handleRequest(array(
+    'processors_path' => $path,
+    'location' => '',
+));
--- assets/components/babel/js/babel.js
+++ assets/components/babel/js/babel.js
@@ -1,37 +1,96 @@
+Ext.ns('Babel');
 /**
- * Babel
  *
- * Copyright 2010 by Jakob Class <jakob.class@class-zec.de>
- *
- * This file is part of Babel.
- *
- * Babel is free software; you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
- *
- * Babel is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * Babel; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
- * Suite 330, Boston, MA 02111-1307 USA
- *
- * @package babel
+ * @class Babel.Translations
+ * @extends Ext.SplitButton
+ * @param {Object} config An object of options.
+ * @xtype babel-translations
  */
-/**
- * Babel JavaScript file for the babel-box in the manager.
- * 
- * @author Jakob Class <jakob.class@class-zec.de>
- *
- * @package babel
- */
-Ext.onReady(function() {
-	Ext.select('div.babel-language:has(.babel-language-layer)').on('mouseenter', function(){
-		Ext.get(this).child('.babel-language-layer').show();
-	})
-	.on('mouseleave', function(){
-		Ext.get(this).child('.babel-language-layer').hide();
-	});
+Babel.Translations = function(config) {
+    config = Babel.config || {};
+    //console.log(config);
+
+    Ext.applyIf(config,{
+        text: 'Translations'
+        ,cls: 'x-btn-text bmenu'
+        ,menu: this.buildTranslations(config)
+//        ,menu: [{
+//            text: 'Language 1'
+//        }, {
+//            text: 'Language 2'
+//        }]
+        //,handler: optionsHandler
+    });
+    Babel.Translations.superclass.constructor.call(this, config);
+};
+
+Ext.extend(Babel.Translations, Ext.SplitButton, {
+    buildTranslations: function(cfg) {
+        var menu = [];
+
+        Ext.each(cfg.translations, function(translation) {
+            console.log(translation);
+
+            var subItems = [];
+            var submenu = {
+                items: subItems
+            };
+            if (translation.showLayer == "yes") {
+                if (translation.showTranslateButton == "yes") {
+                    subItems.push({
+                        text:'Create translation'
+                    })
+                }
+                if (translation.showSecondRow == "yes") {
+                    subItems.push({
+                        text:'Link translation'
+                    })
+                }
+                if (translation.showUnlinkButton == "yes") {
+                    subItems.push({
+                        text:'Unlink translation'
+                        ,scope: this
+                        ,handler: function() {
+                            //console.log(this);
+                            MODx.Ajax.request({
+                                url: Babel.config.connector_url
+                                ,params: {
+                                    action: 'mgr/test'
+                                    //,id: this.config.team
+                                }
+                                ,listeners: {
+                                    success: {
+                                        fn: function(r) {
+                                            console.log('succes');
+                                            console.log(r);
+                                        }
+                                        ,scope: this
+                                    }
+                                }
+                            });
+                        }
+                    })
+                }
+            }
+
+            menu.push({
+                text: translation.cultureKey
+                //text: _('babel.language_fr')
+                ,disabled: (translation.className == "selected") ? true : false
+                ,menu: (subItems.length >= 1) ? submenu : ''
+                ,scope: this
+                ,handler: function() {
+                    //console.log(this);
+                    location.href = '?a='+ MODx.request.a +'&id='+ this.resourceId;
+                }
+            });
+        });
+
+        return menu;
+    }
+
+    ,unlink: function(translation) {
+        console.log('trigger!');
+    }
 });
+Ext.reg('babel-translations', Babel.Translations);
